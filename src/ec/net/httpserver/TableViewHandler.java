6aa44014e80d45a98ff930833106b5e3fc6c1b8f
march1cat <march1cat@gmail.com>
2019-01-10 16:17:31 +0800

    update ectable server side code.

--- src/ec/net/httpserver/TableViewHandler.java
+++ src/ec/net/httpserver/TableViewHandler.java
@@ -1,10 +1,26 @@
 package ec.net.httpserver;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import ec.net.httpserver.ecrtable.EcRenderTable;
 import ec.parser.JsonFactory;
+import ec.system.DeveloperMode;
 
 public abstract class TableViewHandler extends ClassServiceHandler{
 
+	public static final String RES_RESULT_KEY = "result";
+	public static final String RES_RESULT_STATUS_SUCCESS = "ok";
+	public static final String RES_RESULT_STATUS_FAIL = "fail";
+	
+	public static final String RES_MESSAGE_KEY = "message";
+	
+	public static final String RES_DATA_KEY = "data";
+	public static final String RES_DATA_STATUS_EMPTY = "empty";
+	
+	
 	private EcRenderTable bindDefEcTable = null;
 	
 	public TableViewHandler(HttpClientRequest request, EcHttpServer httpServer) {
@@ -13,48 +29,97 @@ public abstract class TableViewHandler extends ClassServiceHandler{
 	
 	
 	public void querySchema() {
-		JsonFactory resData = new JsonFactory();
-		try {
-			resData.setJSONVariable("result", "ok");
-			resData.setJSONVariable("data", bindDefEcTable().getColumnInfos());
-			resData.setJSONVariable("allowupdate", bindDefEcTable().isAllowUpdate() ? "true" : "false");
-			log(resData.encodeJSON());
-			this.response(resData.encodeJSON());
-		} catch (Exception e) {
-			this.exportExceptionText(e);
-			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
-			this.response("{\"result\":\"fail\"}");
-		}
+		Map<String,Object> extraData = new HashMap<>();
+		extraData.put("allowupdate", bindDefEcTable().isAllowUpdate() ? "true" : "false");
+		this.responseSuccessWithQueryData( bindDefEcTable().getColumnInfos(),extraData);
 	}
 	
 	public void onOperationMethodNotDefined(){
-		JsonFactory resData = new JsonFactory();
-		try {
-			resData.setJSONVariable("result", "fail");
-			resData.setJSONVariable("message", "not defined ec table method");
-			this.response(resData.encodeJSON());
-		} catch (Exception e) {
-			this.exportExceptionText(e);
-			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
-			this.response("{\"result\":\"fail\"}");
-		}
+		//wait to override
+		responseFailWithMessage("not defined ec table method");
 	}
 	
+
 	public void commitUpdate() {
+		//wait to override
+		this.responseFailWithMessage("operation(commit update) not allow!!");
+	}
+	
+	protected void responseSuccess(){
+		this.response("{\""+RES_RESULT_KEY+"\":\""+RES_RESULT_STATUS_SUCCESS+"\"}");
+	}
+	
+	protected void responseSuccessWithQueryData(List<Map<Object,String>> data){
 		JsonFactory resData = new JsonFactory();
 		try {
-			resData.setJSONVariable("result", "fail");
-			resData.setJSONVariable("message", "operation(commit update) not allow!!");
+			resData.setJSONVariable(RES_RESULT_KEY, RES_RESULT_STATUS_SUCCESS);
+			resData.setJSONVariable(RES_DATA_KEY, this.isListWithContent(data) ? data : RES_DATA_STATUS_EMPTY);
 			this.response(resData.encodeJSON());
 		} catch (Exception e) {
 			this.exportExceptionText(e);
 			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
-			this.response("{\"result\":\"fail\"}");
+			repsoneFailOnException();
 		}
 	}
 	
-	public abstract void queryData();
-	public abstract void onParamResovleFail();
+	
+	
+	protected void responseSuccessWithQueryData(List<Map<Object,String>> data,Map<String,Object> extraData){
+		JsonFactory resData = new JsonFactory();
+		try {
+			resData.setJSONVariable(RES_RESULT_KEY, RES_RESULT_STATUS_SUCCESS);
+			resData.setJSONVariable(RES_DATA_KEY, this.isListWithContent(data) ? data : RES_DATA_STATUS_EMPTY);
+			Iterator<String> iter = extraData.keySet().iterator();
+			while(iter.hasNext()){
+				String key = iter.next();
+				Object value =  extraData.get(key);
+				if(value != null) resData.setJSONVariable(key, value);
+			}
+			this.response(resData.encodeJSON());
+		} catch (Exception e) {
+			this.exportExceptionText(e);
+			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
+			repsoneFailOnException();
+		}
+	}
+	
+	
+	protected void responseSuccessWithQueryData(Map<String,String> data){
+		JsonFactory resData = new JsonFactory();
+		try {
+			resData.setJSONVariable(RES_RESULT_KEY, RES_RESULT_STATUS_SUCCESS);
+			resData.setJSONVariable(RES_DATA_KEY, data != null ? data : RES_DATA_STATUS_EMPTY);
+			this.response(resData.encodeJSON());
+		} catch (Exception e) {
+			this.exportExceptionText(e);
+			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
+			repsoneFailOnException();
+		}
+	}
+	
+	protected void responseFailWithMessage(String message){
+		JsonFactory resData = new JsonFactory();
+		try {
+			resData.setJSONVariable(RES_RESULT_KEY, RES_RESULT_STATUS_FAIL);
+			resData.setJSONVariable(RES_MESSAGE_KEY, message);
+			this.response(resData.encodeJSON());
+		} catch (Exception e) {
+			this.exportExceptionText(e);
+			this.except("Prepare Response Json Data Fail,Error = " + e.getMessage());
+			repsoneFailOnException();
+		}
+	}
+	
+	
+	protected void repsoneFailOnException(){
+		this.response("{\""+RES_RESULT_KEY+"\":\""+RES_RESULT_STATUS_FAIL+"\"}");
+	}
+	
+	
+	public void onParamResovleFail() {
+		this.except("Parsing Client Query Data Fail on Query Ec Table Data");
+		this.repsoneFailOnException();
+	}
 	
 	
 
@@ -66,6 +131,6 @@ public abstract class TableViewHandler extends ClassServiceHandler{
 		this.bindDefEcTable = bindDefEcTable;
 	}
 	
-	
+	public abstract void queryData();
 
 }
